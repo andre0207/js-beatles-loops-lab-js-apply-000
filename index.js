5682fa2dd27f2281f81d7cc95b4b2045b1d8ea3a
Andre Pierre <andre.laurence94@gmail.com>
2020-07-29 23:04:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 const theBeatlesPlay = (musicians, instruments) => {
   let newArr = [];
   for (let i = 0; i < musicians.length; i++) {
-  
+  newArr.push(`${musicians[i]} plays ${instruments[i]}`)
       
     
   }
-  return newArr.push(`${musicians[i]} plays ${instruments[i]}`)
+  return newArr
 }
