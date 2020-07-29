8ad998b22c6d3402d5727923a5d9ae80da5ccc9f
Andre Pierre <andre.laurence94@gmail.com>
2020-07-29 23:29:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const theBeatlesPlay = (musicians, instruments) => {
   return newArr;
 };
 
-const johnLennonFacts = (Ar) => {
+const johnLennonFacts = (array) => {
   let i = 0
   arrFacts = [];
   while (i < facts.length) {
